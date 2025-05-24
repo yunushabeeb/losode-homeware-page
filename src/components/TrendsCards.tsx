e9c2f969048834697186086df59208c7939f7a64
Yunus Habeeb <habeebdh1@outlook.com>
2025-05-24 09:18:47 +0100

    feat: add custom font styles and integrate new font files for improved typography

--- src/components/TrendsCards.tsx
+++ src/components/TrendsCards.tsx
@@ -5,7 +5,7 @@ import TrendCard from './TrendCard';
 const TrendsCards = () => {
   return (
     <Container bg="bg-white">
-      <div className="flex flex-row flex-wrap justify-between gap-6">
+      <div className="flex flex-row flex-wrap justify-between gap-6 mb-10">
         {trends.map((trend) => (
           <TrendCard
             key={trend.id}
