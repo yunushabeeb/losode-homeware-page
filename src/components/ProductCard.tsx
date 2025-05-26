c8efc597964ca0ccf3f0d25460a66c77b8d21cdb
Yunus Habeeb <habeebdh1@outlook.com>
2025-05-26 12:33:26 +0100

    feat: improve styling of Content and ProductCard components for better readability

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -16,7 +16,9 @@ const ProductCard = ({
       <div className="bg-[#F9F9F9]">
         <img src={image} alt={title} />
       </div>
-      <h2 className="uppercase truncate mt-3">{title}</h2>
+      <h2 className="uppercase text-xl tracking-wider truncate mt-5 mb-2">
+        {title}
+      </h2>
       <p className="w-full font-light truncate">{description}</p>
       <div className="flex justify-between items-center mt-4">
         <a href={href} className="underline">
